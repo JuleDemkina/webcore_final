bd1f8591b02be0f1c0b4b2c1802e9376942a0ca9
Julia Demkina <demkina.yulia@mail.ru>
2024-01-29 18:56:55 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "@babel/polyfill";
+import './index.html';
+import './index.scss';
+// import code from './img/Group.svg'
+
+// const imgWrap = document.querySelector('.img')
+// const img = new Image();
+// img.src = code;
+// img.width = 700;
+// imgWrap.append(img);
